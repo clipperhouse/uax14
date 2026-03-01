19f9cf15222accef27318f96f23568583fde47aa
Matt Sherman <mwsherman@gmail.com>
2026-03-01 13:40:52 -0500

    LB28

--- splitfunc.go
+++ splitfunc.go
@@ -403,6 +403,13 @@ func NextBreak[T ~string | ~[]byte](data T) (advance int, kind breakKind) {
 			continue
 		}
 
+		// https://www.unicode.org/reports/tr14/#LB28
+		// (AL | HL) × (AL | HL)
+		if lastExCMZWJ.is(_AL|_HL) && current.is(_AL|_HL) {
+			pos += w
+			continue
+		}
+
 		// https://www.unicode.org/reports/tr14/#LB31
 		// Default break opportunity
 		return pos, breakOpportunity
